0e5a1e40c5cbd372d6d9d194ba2d09cd98c73511
xovaaan <xovan089@gmail.com>
2023-12-25 02:06:20 +0600

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,24 @@
+
+import logo from "../assets/icons.png"
+
+
+const Hero = () => {
+  return (
+    <header className='w-full flex justify-center items-center flex-col'>
+      <nav className='flex justify-center items-center w-full mb-4 mt-8 pt-3'>
+        <img src={logo} alt='sumz_logo' className='w-12 object-contain' />
+
+      </nav>
+
+      <h1 className=' mt-5 text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl text-center'>
+        Summarize Articles with <br className='max-md:hidden' />
+        <span className='title mt-5  '>SummaryAI</span>
+      </h1>
+      <h2 className='mt-5 text-lg text-gray-600 sm:text-xl text-center max-w-2xl'>
+      AI-driven Article Link to Summary technology condenses linked articles into brief, <span className='desc'>informative overviews using natural language processing</span>
+      </h2>
+    </header>
+  );
+};
+
+export default Hero;
