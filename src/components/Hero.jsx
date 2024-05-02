a71e26df0ece5e0bff680f8204732b3b28243311
xovaaan <xovan089@gmail.com>
2024-05-03 02:12:44 +0600

    trss

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -12,7 +12,7 @@ const Hero = () => {
 
       <h1 className=' mt-5 text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl text-center'>
         Summarize Articles with <br className='max-md:hidden' />
-        <span className='title mt-5  '>SummaryAI</span>
+        <span className='title mt-5  '>Linkbase AI</span>
       </h1>
       <h2 className='mt-5 text-lg text-gray-600 sm:text-xl text-center max-w-2xl'>
       AI-driven Article Link to Summary technology condenses linked articles into brief, <span className='desc'>informative overviews using natural language processing</span>
